65160134e02f8d55d48ed48cf5c8363e3de175f5
lukebella <bellaniluca9@gmail.com>
2024-10-22 11:54:01 +0200

    Add fine tuning with IEMOCAP and tested it

--- prova.py
+++ prova.py
@@ -1,12 +1,20 @@
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+
 from vocal_assistant import VocalAssistant
 from emotion_model import process_func, EmotionModel
 from transformers import Wav2Vec2Processor
-
+from predict_emotion import load_trained_model, predict_emotion
 
 device = 'cpu'
-model_name = 'audeering/wav2vec2-large-robust-12-ft-emotion-msp-dim'
-processor = Wav2Vec2Processor.from_pretrained(model_name)
-model = EmotionModel.from_pretrained(model_name).to(device)
+
+audeering_model_name = 'audeering/wav2vec2-large-robust-12-ft-emotion-msp-dim'
+audeering_processor = Wav2Vec2Processor.from_pretrained(audeering_model_name)
+audeering_model = EmotionModel.from_pretrained(audeering_model_name).to(device)
+
+custom_model_name = "model_checkpoint.pth"
+custom_model, custom_processor = load_trained_model(custom_model_name)
+
 
 vc = VocalAssistant(1)
 vc.talk("What is your mood today?")
@@ -15,4 +23,8 @@ while True:
     print(command)
     break
 
+print("Audeering: ")
 print(process_func(vocal_file, 16000))
+print()
+print("Custom: ")
+print(predict_emotion(custom_model, custom_processor, vocal_file))
