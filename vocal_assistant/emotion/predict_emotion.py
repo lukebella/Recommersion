17383bf62288977e7df6b5f8125dd7d7c64a6c11
lukebella <bellaniluca9@gmail.com>
2024-10-28 12:46:59 +0100

    Modifying simple regression network: still the same problem

--- vocal_assistant/emotion/predict_emotion.py
+++ vocal_assistant/emotion/predict_emotion.py
@@ -1,7 +1,7 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
-from transformers import Wav2Vec2Model, Wav2Vec2Processor
+from transformers import Wav2Vec2Model, Wav2Vec2Processor, Wav2Vec2PreTrainedModel, Wav2Vec2Config
 import torch.nn as nn
 from torch.optim import AdamW
 from tqdm import tqdm
@@ -28,22 +28,74 @@ class EmotionDataset(Dataset):
         inputs['labels'] = torch.tensor([valence, arousal, dominance], dtype=torch.float32)
         
         return inputs
+    
 
-class Wav2Vec2ForEmotionRegression(nn.Module):
+class RegressionHead(nn.Module):
+    r"""Classification head."""
+
+    def __init__(self, config):
+
+        super().__init__()
+
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        self.dropout = nn.Dropout(config.final_dropout)
+        self.out_proj = nn.Linear(config.hidden_size, config.num_labels)
+
+    def forward(self, features, **kwargs):
+
+        x = features
+        x = self.dropout(x)
+        x = self.dense(x)
+        x = torch.tanh(x)
+        x = self.dropout(x)
+        x = self.out_proj(x)
+
+        return x
+
+
+class EmotionModel(Wav2Vec2PreTrainedModel):
+    r"""Speech emotion classifier."""
+
+    def __init__(self, config):
+
+        super().__init__(config)
+
+        self.config = config
+        self.wav2vec2 = Wav2Vec2Model(config)
+        self.classifier = RegressionHead(config)
+        self.init_weights()
+
+    def forward(
+            self,
+            input_values,
+            attention_mask=None
+    ):
+
+        outputs = self.wav2vec2(input_values, attention_mask=attention_mask)
+        hidden_states = outputs[0]
+        hidden_states = torch.mean(hidden_states, dim=1)
+        logits = self.classifier(hidden_states)
+
+        return hidden_states, logits
+    
+
+
+"""class Wav2Vec2ForEmotionRegression(nn.Module):
     def __init__(self):
         super(Wav2Vec2ForEmotionRegression, self).__init__()
         self.wav2vec2 = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base")
         self.rnn = nn.LSTM(input_size=self.wav2vec2.config.hidden_size, hidden_size=128, num_layers=2, batch_first=True, bidirectional=True)
+        self.dropout = nn.Dropout()
         self.regression_layer = nn.Linear(128 * 2, 3)  # 128 * 2 because it's bidirectional
-
+    
     def forward(self, input_values, attention_mask=None):
         outputs = self.wav2vec2(input_values=input_values, attention_mask=attention_mask)
-        hidden_states = outputs.last_hidden_state
-        rnn_output, _ = self.rnn(hidden_states)
-        
-        # Average pooling over the time dimension
-        pooled_output = rnn_output.mean(dim=1)
-        return self.regression_layer(pooled_output)
+        hidden_states = outputs[0]
+        hidden_states = torch.mean(hidden_states, dim=1)
+        drop_out = self.dropout(hidden_states)
+        rnn_output, _ = self.rnn(drop_out)
+
+        return self.regression_layer(rnn_output)"""
 
 def custom_collate(batch):
     input_values = [item['input_values'].squeeze(0) for item in batch]
@@ -69,7 +121,7 @@ def save_checkpoint(model, optimizer, epoch, filename):
     torch.save(checkpoint, filename)
     print(f"Checkpoint saved at epoch {epoch + 1}")
 
-def load_checkpoint(model, optimizer, filename):
+"""def load_checkpoint(model, optimizer, filename):
     device = return_device()
     if os.path.isfile(filename):
         checkpoint = torch.load(filename, map_location=device)  
@@ -80,7 +132,7 @@ def load_checkpoint(model, optimizer, filename):
         return start_epoch
     else:
         print("No checkpoint found, starting from scratch.")
-        return 0
+        return 0"""
     
 def return_device():
     torch.device("cuda" if torch.cuda.is_available() else "cpu")  # Dynamically set device
@@ -103,8 +155,10 @@ def train(model, train_dataloader, test_dataloader, epochs=3):
             labels = batch['labels'].to(device)
             
             optimizer.zero_grad()
-            outputs = model(input_values=input_values, attention_mask=attention_mask)
-            loss = loss_fn(outputs, labels)
+            #outputs = model(input_values=input_values, attention_mask=attention_mask)
+            #loss = loss_fn(outputs, labels)
+            _, logits = model(input_values=input_values, attention_mask=attention_mask)
+            loss = loss_fn(logits, labels)
             loss.backward()
             optimizer.step()
             
@@ -122,8 +176,10 @@ def train(model, train_dataloader, test_dataloader, epochs=3):
                 attention_mask = batch['attention_mask'].to(device)
                 labels = batch['labels'].to(device)
                 
-                outputs = model(input_values=input_values, attention_mask=attention_mask)
-                loss = loss_fn(outputs, labels)
+                #outputs = model(input_values=input_values, attention_mask=attention_mask)
+                #loss = loss_fn(outputs, labels)
+                _, logits = model(input_values=input_values, attention_mask=attention_mask)
+                loss = loss_fn(logits, labels)
                 val_loss += loss.item()
 
         print(f"Epoch {epoch + 1}/{epochs}, Validation Loss: {val_loss / len(test_dataloader)}")
@@ -131,7 +187,10 @@ def train(model, train_dataloader, test_dataloader, epochs=3):
 
 def load_trained_model(checkpoint_path):
     device = return_device()
-    model = Wav2Vec2ForEmotionRegression().to(device)
+    #model = Wav2Vec2ForEmotionRegression().to(device)
+    config = Wav2Vec2Config.from_pretrained("facebook/wav2vec2-base")
+    config.num_labels = 3  # Ensure this matches the number of regression outputs (Valence, Arousal, Dominance)
+    model = EmotionModel(config).to(device)
     processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base")
     
     if os.path.isfile(checkpoint_path):
@@ -144,28 +203,42 @@ def load_trained_model(checkpoint_path):
     return model, processor
 
 def predict_emotion(model, processor, wav_data):
+    device = return_device()
     model.eval()
     inputs = processor(wav_data, sampling_rate=16000, return_tensors="pt", padding=True)
-    input_values = inputs['input_values'].to("cpu")
-    attention_mask = inputs.get('attention_mask').to("cpu") if 'attention_mask' in inputs else None
+    input_values = inputs['input_values'].to(device)
+    attention_mask = inputs.get('attention_mask').to(device) if 'attention_mask' in inputs else None
 
     with torch.no_grad():
         outputs = model(input_values=input_values, attention_mask=attention_mask)
     
-    valence, arousal, dominance = outputs.squeeze().tolist()
+    """valence, arousal, dominance = outputs.squeeze().tolist()
     return {
         "Valence": valence,
         "Arousal": arousal,
         "Dominance": dominance
-    }
+    }"""
+    return outputs[1]
 
 if __name__ == "__main ":
     processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base")
-    model = Wav2Vec2ForEmotionRegression().to(return_device())
+    #model = Wav2Vec2ForEmotionRegression().to(return_device())
+    config = Wav2Vec2Config.from_pretrained("facebook/wav2vec2-base")
+    config.num_labels = 3  # Ensure this matches the number of regression outputs (Valence, Arousal, Dominance)
+    model = EmotionModel(config).to(return_device())
 
     df = pd.read_pickle("data/IEMOCAP_useful")
     df_shuffled = df.sample(frac=1, random_state=42).reset_index(drop=True)
-    df_sampled = df_shuffled.sample(frac=0.01, random_state=42)
+    print(df_shuffled.head())
+    """
+                    Turn_Name  Valence  Arousal  Dominance                                           wav_file
+    0  Ses04F_script01_2_F020      0.3      0.7        0.8  [0.00045776367, -3.0517578e-05, 0.00045776367,...
+    1  Ses02M_script02_1_F010      0.5      0.5        0.5  [-0.0032653809, -0.003112793, -0.0029296875, -...
+    2  Ses05F_script01_3_M030      0.5      0.8        0.9  [-0.00045776367, -0.00079345703, -0.0007019043...
+    3  Ses04M_script01_3_M002      0.5      0.5        0.6  [-0.0032043457, -0.0033569336, -0.003479004, -...
+    4  Ses05F_script02_2_F023      0.4      0.7        0.7  [0.011016846, -0.017822266, -0.0079956055, 0.0...
+    """
+    df_sampled = df_shuffled.sample(frac=0.02, random_state=42)
 
     train_df, test_df = train_test_split(df_sampled, test_size=0.2, random_state=42)
 
