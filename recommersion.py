a0dd03ca122dc417b5027749229a6359b49c64ed
lukebella <luca.bellani@studenti.unimi.it>
2025-02-14 10:30:23 +0100

    Updated interface

--- recommersion.py
+++ recommersion.py
@@ -1,12 +1,13 @@
+
+import requests
 import tkinter as tk
 import customtkinter as ctk
 from tkinter import messagebox, ttk
 from vocal_assistant.vocal_assistant import VocalAssistant
 from vocal_assistant.emotion.emotion_model import process_func
 from vocal_assistant.emotion.predict_emotion import load_trained_model, predict_emotion, return_device
-from vocal_assistant.emotion.emotion_model import process_func
 import pandas as pd
-import numpy as np
+from numpy import linalg
 import threading
 import pickle
 import pygame
@@ -15,7 +16,7 @@ from hover_interface.hover_text import HoverText
 from PIL import Image
 import validators
 import tempfile 
-import requests
+import sounddevice as sd
 
 
 # Appearence and default color
@@ -40,7 +41,7 @@ class Functions:
         self.custom_model, self.custom_processor = load_trained_model(self.device, \
                                                                       self.custom_model_name, self.custom_pretrained_model)
         
-        with open("./data/SpotiGEM_songs", "rb") as f:
+        with open("./data/Songs_path", "rb") as f:  #SpotiGEM_songs
             self.data = pickle.load(f)
         print("Songs loaded!")
         self.callback()
@@ -79,9 +80,9 @@ class Functions:
        
     
     # Generating playlist via the selected distance
-    def generate_playlist(self, dimensional, distance:str,  cut:int = 10):
+    def generate_playlist(self, dimensional, distance:str,  cut:int = 5):
         dist_dict = {
-            "Euclidean": lambda x, y: np.linalg.norm(x-y),
+            "Euclidean": lambda x, y: linalg.norm(x-y),
             "Cosine": lambda x, y: cosine(x, y)
         }
         dist_func = dist_dict[distance]
@@ -118,6 +119,8 @@ class Recommersion(ctk.CTk):
         self.current_song = pd.DataFrame([])
         self.paused = True
         
+        self.current_speech = None
+
         # Class for display text while hovering any widget 
         self.hover_text = HoverText("hover_interface/widget_hover.txt")
         
@@ -144,7 +147,7 @@ class Recommersion(ctk.CTk):
     def create_input_frame(self):
         self.input_frame = ctk.CTkFrame(self, width=int(self.window_width*0.4), corner_radius=0, fg_color="gray5", bg_color="white")
         self.input_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
-        self.input_frame.grid_rowconfigure(9, weight=1)
+        self.input_frame.grid_rowconfigure(11, weight=1)
         self.input_frame.grid_columnconfigure(2, weight=1)
 
         # Voice Input
@@ -174,42 +177,52 @@ class Recommersion(ctk.CTk):
         self.menu.bind("<Enter>", lambda event: self.hover_show(event, "menu"))
         self.model.trace_add("write", self.on_option_change)
 
+        self.speech_button = ctk.CTkButton(self.input_frame, text="Reproduce your speech", command=self.reproduce_speech, \
+                                        fg_color="SlateBlue4")
+        self.speech_button.grid(row=3, column=1, padx=(self.padding_width,self.padding_width*2), pady=self.padding_height, sticky="nsew")
+        self.speech_button.bind("<Enter>", lambda event: self.hover_show(event, "speech_button"))
+
+        self.generate = ctk.CTkButton(self.input_frame, text="Generate", command=self.generate_from_speech, \
+                                        fg_color="SlateBlue4")
+        self.generate.grid(row=4, column=1, padx=(self.padding_width,self.padding_width*2), pady=self.padding_height, sticky="nsew")
+        self.generate.bind("<Enter>", lambda event: self.hover_show(event, "generate"))
+
         # Parameters Input
         self.parameters_label = ctk.CTkLabel(self.input_frame, text="Parameters", font=ctk.CTkFont(size=20, weight="bold"))
-        self.parameters_label.grid(row=3, column=0, padx=self.padding_width, pady=(self.padding_height*5, self.padding_height))
+        self.parameters_label.grid(row=5, column=0, padx=self.padding_width, pady=(self.padding_height*5, self.padding_height))
         self.cut_label = ctk.CTkLabel(self.input_frame, text="Number of Songs:", font=("Arial", 16), anchor="w")
-        self.cut_label.grid(row=4, column=0, padx=self.padding_width, pady=self.padding_height)
+        self.cut_label.grid(row=6, column=0, padx=self.padding_width, pady=self.padding_height)
 
-        self.cut_text = ctk.StringVar(value="10")  
+        self.cut_text = ctk.StringVar(value="5")  
         self.cut = ctk.CTkEntry(self.input_frame, placeholder_text=self.cut_text.get(), textvariable=self.cut_text)
-        self.cut.grid(row=4, column=1, padx=(self.padding_width,self.padding_width*2), pady=self.padding_height, sticky="nsew")
+        self.cut.grid(row=6, column=1, padx=(self.padding_width,self.padding_width*2), pady=self.padding_height, sticky="nsew")
         self.cut.bind("<Enter>", lambda event: self.hover_show(event, "cut"))
         self.cut_text.trace_add("write", self.on_text_change)
 
         self.distance_label = ctk.CTkLabel(self.input_frame, text="Distance", font=("Arial", 16), anchor="w")
-        self.distance_label.grid(row=5, column=0, padx=self.padding_width, pady=self.padding_height)
+        self.distance_label.grid(row=7, column=0, padx=self.padding_width, pady=self.padding_height)
 
         self.DISTANCE_OPTIONS = ["Euclidean", "Cosine"]
         self.distance = ctk.StringVar(value=self.DISTANCE_OPTIONS[0])  
         self.menu_distance = ctk.CTkOptionMenu(self.input_frame, values = self.DISTANCE_OPTIONS, \
                                                variable=self.distance, command = self.on_option_change_distance, \
                                                fg_color="SlateBlue4", dropdown_fg_color="SlateBlue4")
-        self.menu_distance.grid(row=5, column=1, padx=(self.padding_width,self.padding_width*2), pady=self.padding_height, sticky="nsew")
+        self.menu_distance.grid(row=7, column=1, padx=(self.padding_width,self.padding_width*2), pady=self.padding_height, sticky="nsew")
         self.menu_distance.bind("<Enter>", lambda event: self.hover_show(event, "menu_distance"))
         self.distance.trace_add("write", self.on_option_change_distance)
 
         # Instruction Textbox
         self.instr_label = ctk.CTkLabel(self.input_frame, text = "Instructions",font=ctk.CTkFont(size=20, weight="bold"))
-        self.instr_label.grid(row=7, column=0, padx=(self.padding_width*2, self.padding_width*2), pady=(self.padding_height*16, self.padding_height), sticky="w")
+        self.instr_label.grid(row=9, column=0, padx=(self.padding_width*2, self.padding_width*2), pady=(self.padding_height*16, self.padding_height), sticky="w")
         self.textbox = ctk.CTkTextbox(self.input_frame, wrap = "word", height=int(self.window_height*0.10), width=int(self.window_width*0.15), font=ctk.CTkFont("italic", size=12))
         self.textbox.insert(1.0, self.hover_text.get_widget("general"))
-        self.textbox.grid(row=8, column=0, padx=(self.padding_width*2, self.padding_width*2), pady=(self.padding_height*2, self.padding_height+3), sticky="nsew")
+        self.textbox.grid(row=10, column=0, padx=(self.padding_width*2, self.padding_width*2), pady=(self.padding_height*2, self.padding_height+3), sticky="nsew")
 
         # Quadrant frame
         self.quadrant_label = ctk.CTkLabel(self.input_frame, text = "Emotion Quadrants",font=ctk.CTkFont(size=20, weight="bold"))
-        self.quadrant_label.grid(row=7, column=1, padx=(self.padding_width*2, self.padding_width*2), pady=(self.padding_height*20, self.padding_height), sticky="nsew")
+        self.quadrant_label.grid(row=9, column=1, padx=(self.padding_width*2, self.padding_width*2), pady=(self.padding_height*20, self.padding_height), sticky="nsew")
         self.quadrant_frame = ctk.CTkFrame(self.input_frame, height=int(self.window_height*0.3), width=int(self.window_width*0.25), fg_color="white")
-        self.quadrant_frame.grid(row=8, column=1, padx=(self.padding_width, self.padding_width*2), pady=(self.padding_height, self.padding_height), sticky="nsew")
+        self.quadrant_frame.grid(row=10, column=1, padx=(self.padding_width, self.padding_width*2), pady=(self.padding_height, self.padding_height), sticky="nsew")
         self.quadrant_frame.bind("<Enter>", lambda event: self.hover_show(event, "quadrant_frame"))
         self.image = ctk.CTkImage(light_image = Image.open("hover_interface/emotion_quadrant.drawio.png"),\
                                   dark_image = Image.open("hover_interface/emotion_quadrant.drawio.png"),
@@ -390,14 +403,25 @@ class Recommersion(ctk.CTk):
     
     # Trigger when Microphone button is pressed
     def start_microphone(self):
-        if self.data_loaded:
-            messagebox.showinfo("Microphone", "Microphone started! Please speak.")
-            self.pause_song()
-            command, speech = self.functions.start_microphone()
-            self.speech_var.set(value = command) 
-            speech_array = self.functions.process_audio_file(speech)
-            dimensional = self.functions.predict_valence_arousal(file = speech_array, \
-                                                                 model = self.model.get())
+        messagebox.showinfo("Microphone", "Microphone started! Please speak.")
+        self.pause_song()
+        command, speech = self.functions.start_microphone()
+        self.speech_var.set(value = command) 
+        speech_array = self.functions.process_audio_file(speech)
+        self.current_speech = speech_array
+        
+
+    def reproduce_speech(self):
+        if not(self.current_speech is None or (self.mixer.music.get_busy())):
+            sd.play(self.current_speech, 16000)
+        else: 
+            messagebox.showinfo("Speech", "No recorded speech yet!")
+
+
+    def generate_from_speech(self):
+        if self.current_speech is not None and self.data_loaded:
+            dimensional = self.functions.predict_valence_arousal(file = self.current_speech, \
+                                                                model = self.model.get())
             print(dimensional)
             print("before setting")
             new_val = dimensional[0] * self.valence_slider.cget("to")
@@ -409,8 +433,9 @@ class Recommersion(ctk.CTk):
             print("after setting")
             self.text_var.set("Loading playlist: please wait a moment...")
             self.compute_playlist(dimensional)
-        else:
-            messagebox.showinfo("Microphone", "No songs to compute yet")
+        else: 
+            messagebox.showinfo("System", "No recorded speech or songs not loaded!")
+
 
 
     # Playlist creation or updating
@@ -526,24 +551,3 @@ if __name__ == "__main__":
     app.mainloop()
 
 
-
-#Tests:
-    #Try 3 emotion status
-    #For Precision
-    #Cut = 5
-        #Custom/Euclidean
-        #Custom/Cosine
-        #Audeering/Euclidean
-        #Audeering/Cosine
-    #Cut = 10
-        #Custom/Euclidean
-        #Custom/Cosine
-        #Audeering/Euclidean
-        #Audeering/Cosine
-    #For every test, check how much it is deviated with the sliders until the user finds the song
-    #appropriate for his/her mood
-    #General questions:
-        #Do you find Recommersion useful in a music stream platform?
-        #If yes, would you change something?
-    
-    #Calculate distance of the first song based on the request
